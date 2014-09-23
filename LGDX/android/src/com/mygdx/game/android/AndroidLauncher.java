9fc047faefc71aa009d8a30c0c587f2a26f91d27
Ian Malerich <ian.malerich@outlook.com>
2014-09-22 21:06:01 -0400

    Modified formatting AndroidLauncher.java

--- LGDX/android/src/com/mygdx/game/android/AndroidLauncher.java
+++ LGDX/android/src/com/mygdx/game/android/AndroidLauncher.java
@@ -8,7 +8,8 @@ import com.mygdx.game.Game;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
-	protected void onCreate (Bundle savedInstanceState) {
+	protected void onCreate (Bundle savedInstanceState) 
+	{
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new Game(), config);
